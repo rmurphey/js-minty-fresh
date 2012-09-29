a76623abb16a95cb1f57d50543b3e2445dc4156e
Rebecca Murphey <rmurphey@gmail.com>
2012-09-29 18:13:46 -0400

    numbered things example

---
+++ examples/numbered-things/busted.js
@@ -0,0 +1,18 @@
+function showPeople(people) {
+  $.each(people, function(idx, person) {
+    $('<li>', {
+      id: 'person_' + person.id,
+      text: person.name
+    }).appendTo('#people');
+
+    $('<button>', {
+      id: 'button_' + person.id,
+      text: 'Remove ' + person.name,
+      click: function(e) {
+        e.preventDefault();
+        var id = $(this).attr('id').replace('button_', '');
+        $('#person_' + id).remove();
+      }
+    }).appendTo('#buttons');
+  });
+}
