9ddd2dab79bd1fadf63311a6d4e3f5c691024fb3
Rebecca Murphey <rmurphey@gmail.com>
2012-10-01 20:33:50 -0400

    better template opportunity example

--- examples/templates/opportunity.js
+++ examples/templates/opportunity.js
@@ -1,9 +1,10 @@
-function getTemplate(name) {
+function getTemplate( name ) {
   window._templateCache = window._templateCache || {};
 
-  return window._templateCache[ name ] ||
-    $.get('/templates/' + name).pipe(function(tmpl) {
-      var compiled = window._templateCache[ name ] = _.template( tmpl );
-      return compiled;
-    });
+  if ( !window._templateCache[ name ] ) {
+    window._templateCache[ name ] =
+      $.get( '/templates/' + name ).pipe( _.template );
+  }
+
+  return window._templateCache[ name ];
 }
