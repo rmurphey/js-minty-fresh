89b09078d0a6540446540779fec9f769e5a6d74c
Rebecca Murphey <rmurphey@gmail.com>
2012-10-03 18:04:03 +0200

    switch to object/function solution

--- examples/dry-functions/minty.js
+++ examples/dry-functions/minty.js
@@ -1,12 +1,13 @@
 function updateDates() {
-  updateDate( $('#menu-item-1'), $('#start-date') );
-  updateDate( $('#menu-item-2'), $('#end-date') );
-
-  function updateDate( target, datepicker ) {
-    var date = datepicker.datepicker( 'getDate' );
+  $.each({
+    '#menu-item-1': '#start-date',
+    '#menu-item-2': '#end-date'
+  }, function( menuItem, datepicker ) {
+    var target = $(menuItem);
+    var date = $(datepicker).datepicker( 'getDate' );
 
     target.find( '.day' ).text( date.getDate() );
     target.find( '.month' ).text( Util.monthToText( date.getMonth() ) );
     target.find( '.year' ).text( date.getFullYear() );
-  }
+  });
 }
