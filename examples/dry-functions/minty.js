09f56c80b7c13f4127a464c9bd8583441f045143
Rebecca Murphey <rmurphey@gmail.com>
2012-10-04 19:41:43 +0200

    formatting

--- examples/dry-functions/minty.js
+++ examples/dry-functions/minty.js
@@ -3,8 +3,8 @@ function updateDates() {
     '#menu-item-1': '#start-date',
     '#menu-item-2': '#end-date'
   }, function( menuItem, datepicker ) {
-    var target = $(menuItem);
-    var date = $(datepicker).datepicker( 'getDate' );
+    var target = $( menuItem );
+    var date = $( datepicker ).datepicker( 'getDate' );
 
     target.find( '.day' ).text( date.getDate() );
     target.find( '.month' ).text( Util.monthToText( date.getMonth() ) );
