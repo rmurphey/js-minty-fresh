e3f995a5b38b68ee2915d50d869b90bfb5137d1d
Rebecca Murphey <rmurphey@gmail.com>
2012-10-05 17:07:10 +0200

    fix typo

--- examples/dry-objects/opportunity.js
+++ examples/dry-objects/opportunity.js
@@ -1,6 +1,6 @@
 var selected = document.getElementById( "countyLocation" ).value;
 
-$.get( '/data/locations.json?q=' + selected, function( location ) {
+$.getJSON( '/data/locations.json?q=' + selected, function( location ) {
   var center = new google.maps.LatLng( location.lat, location.lng );
   map.setCenter( center );
   map.setZoom( location.zoom );
